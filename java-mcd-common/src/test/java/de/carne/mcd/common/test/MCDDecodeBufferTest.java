ccc7dd70502dc71a56b0c29634f074d824bc3072
Holger <hdecarne@gmail.com>
2019-12-04 22:12:42 +0100

    Addd test code

--- java-mcd-common/src/test/java/de/carne/mcd/common/test/MCDDecodeBufferTest.java
+++ java-mcd-common/src/test/java/de/carne/mcd/common/test/MCDDecodeBufferTest.java
@@ -83,7 +83,7 @@ class MCDDecodeBufferTest {
 	}
 
 	@Test
-	void testDecodeScalars() throws IOException {
+	void testDecodeValues() throws IOException {
 		try (ReadableByteChannel channel = Channels.newChannel(new ByteArrayInputStream(TEST_BYTES))) {
 			MCDDecodeBuffer buffer = new MCDDecodeBuffer(channel, ByteOrder.BIG_ENDIAN);
 
@@ -93,6 +93,73 @@ class MCDDecodeBufferTest {
 			Assertions.assertEquals(0x0708090a0b0c0d0el, buffer.decodeI64());
 			Assertions.assertEquals(Float.intBitsToFloat(0x0f101112), buffer.decodeF32());
 			Assertions.assertEquals(Double.longBitsToDouble(0x131415161718191al), buffer.decodeF64());
+			Assertions.assertArrayEquals(new byte[] { (byte) 0x1b, (byte) 0x1c },
+					bufferToBytes(buffer.decodeI8Array(2)));
+			Assertions.assertArrayEquals(new short[] { (short) 0x1d1e, (short) 0x1f20 },
+					bufferToShorts(buffer.decodeI16Array(2)));
+			Assertions.assertArrayEquals(new int[] { 0x21222324, 0x25262728 }, bufferToInts(buffer.decodeI32Array(2)));
+			Assertions.assertArrayEquals(new long[] { 0x292a2b2c2d2e2f30l, 0x3132333435363738l },
+					bufferToLongs(buffer.decodeI64Array(2)));
+		}
+	}
+
+	private byte[] bufferToBytes(ByteBuffer buffer) {
+		byte[] bytes = new byte[buffer.remaining()];
+		int byteIndex = 0;
+
+		while (buffer.remaining() > 0) {
+			bytes[byteIndex] = buffer.get();
+			byteIndex++;
+		}
+		return bytes;
+	}
+
+	private short[] bufferToShorts(ByteBuffer buffer) {
+		short[] shorts = new short[buffer.remaining() / Short.BYTES];
+		int shortIndex = 0;
+
+		while (buffer.remaining() > 0) {
+			shorts[shortIndex] = buffer.getShort();
+			shortIndex++;
+		}
+		return shorts;
+	}
+
+	private int[] bufferToInts(ByteBuffer buffer) {
+		int[] ints = new int[buffer.remaining() / Integer.BYTES];
+		int intIndex = 0;
+
+		while (buffer.remaining() > 0) {
+			ints[intIndex] = buffer.getInt();
+			intIndex++;
+		}
+		return ints;
+	}
+
+	private long[] bufferToLongs(ByteBuffer buffer) {
+		long[] longs = new long[buffer.remaining() / Long.BYTES];
+		int longIndex = 0;
+
+		while (buffer.remaining() > 0) {
+			longs[longIndex] = buffer.getLong();
+			longIndex++;
+		}
+		return longs;
+	}
+
+	@Test
+	void testDecodeMagic() throws IOException {
+		try (ReadableByteChannel channel = Channels.newChannel(new ByteArrayInputStream(TEST_BYTES))) {
+			MCDDecodeBuffer buffer = new MCDDecodeBuffer(channel, ByteOrder.BIG_ENDIAN);
+
+			Assertions.assertDoesNotThrow(() -> buffer.decodeMagic((byte) 0x00));
+			Assertions.assertThrows(IOException.class, () -> buffer.decodeMagic((byte) 0x00));
+			Assertions.assertDoesNotThrow(() -> buffer.decodeMagic((short) 0x0203));
+			Assertions.assertThrows(IOException.class, () -> buffer.decodeMagic((short) 0x0203));
+			Assertions.assertDoesNotThrow(() -> buffer.decodeMagic(0x06070809));
+			Assertions.assertThrows(IOException.class, () -> buffer.decodeMagic(0x06070809));
+			Assertions.assertDoesNotThrow(() -> buffer.decodeMagic(0x0e0f101112131415l));
+			Assertions.assertThrows(IOException.class, () -> buffer.decodeMagic(0x0e0f101112131415l));
 		}
 	}
 
