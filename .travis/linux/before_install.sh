4550c3f9bd31cebfaba4bf3a230257a78b750c9a
Holger <hdecarne@gmail.com>
2020-05-03 06:29:32 +0200

    Update JDK prior to test run

--- .travis/linux/before_install.sh
+++ .travis/linux/before_install.sh
@@ -3,4 +3,7 @@
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
 jabba ls-remote
 jabba install $BUILD_JDK
+export JAVA_HOME="$JABBA_HOME/jdk/$BUILD_JDK"
+export PATH="$JAVA_HOME/bin:$PATH"
+java -Xmx32m -version 
 ./gradlew -v
