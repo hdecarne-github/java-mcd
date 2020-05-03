a3214e342d71a798cd76e22d81c9039ed61d9b90
Holger <hdecarne@gmail.com>
2020-05-03 06:21:02 +0200

    Avoid compiler warning caused by forward export

--- java-mcd-default/src/main/java/module-info.java
+++ java-mcd-default/src/main/java/module-info.java
@@ -7,9 +7,8 @@ module de.carne.mcd {
 	requires de.carne;
 
 	exports de.carne.mcd;
-
-	exports de.carne.mcd.bootstrap to de.carne.mcd.jvmdecoder, de.carne.mcd.x86decoder;
-	exports de.carne.mcd.instruction to de.carne.mcd.jvmdecoder, de.carne.mcd.x86decoder;
-	exports de.carne.mcd.io to de.carne.mcd.jvmdecoder, de.carne.mcd.x86decoder;
+	exports de.carne.mcd.bootstrap;
+	exports de.carne.mcd.instruction;
+	exports de.carne.mcd.io;
 
 }
